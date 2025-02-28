a0350d8c9c39ebb5bcf91f7d053ec42ecab76ac5
SamWood-26 <Samuel.wood648693@tufts.edu>
2025-02-27 20:46:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 # main.py
 import streamlit as st
+import os
+import pandas as pd
+from noLLM_analysis import *
+
+
+if "species" not in st.session_state:
+    st.session_state.species = None
+if "tissue" not in st.session_state:
+    st.session_state.tissue = None
+if "marker_genes" not in st.session_state:
+    st.session_state.marker_genes = ""
+if "api_key" not in st.session_state:
+    st.session_state.api_key = ""
+if "background_context" not in st.session_state:
+    st.session_state.background_context = ""
+if "Gene_denominator" not in st.session_state:
+    st.session_state.Gene_denominator = ""
+if "custom_data" not in st.session_state:
+   st.session_state.custom_data = ""
+    
 
 
 st.set_page_config(page_title="Home Page")
@@ -58,3 +78,85 @@ This website employs a straightforward yet flexible approach to cell type predic
    - Using the entered marker genes and contextual tissue information, the AI analyzes the input to predict the cell type that best aligns with the given data.  
    - This method is especially useful when users have predefined options and need additional computational insights to refine their predictions.
 """)
+
+#loading data
+@st.cache_data
+def get_data():
+   df = load_data()
+   df_human = df[df['Species'] == 'Homo sapiens']
+   df_mouse = df[df['Species'] == 'Mus musculus']
+
+   total_cells = df['Cell_standard'].nunique()
+   human_cells = df_human['Cell_standard'].nunique()
+   mouse_cells = df_mouse['Cell_standard'].nunique()
+   return df, df_human, df_mouse, total_cells, human_cells, mouse_cells
+
+#data frames loaded in
+df, df_human, df_mouse, total_cells, human_cells, mouse_cells = get_data()
+
+
+st.session_state.background_context = st.selectbox(
+   "Select Dataset",
+   ["Base","Mouse Liver", "Human Breast Cancer", "Upload TSV File", "Custom Input"]
+)
+
+# File uploader appears only if 'Upload TSV File' is selected
+if st.session_state.background_context == "Upload TSV File":
+    uploaded_file = st.file_uploader("Upload your TSV file", type=["tsv"])
+
+    if uploaded_file:
+        df_uploaded = pd.read_csv(uploaded_file, sep="\t")  # Read TSV file
+        if not df_uploaded.empty:
+            first_column_name = df_uploaded.columns[0]  # Get first column name
+            marker_genes_from_file = df_uploaded[first_column_name].dropna().astype(str).tolist()  # Extract and clean data
+            st.session_state.Gene_denominator = np.array(marker_genes_from_file)  # Store in session state
+
+            st.success(f"Extracted {len(marker_genes_from_file)} marker genes from file.")
+            st.write("Extracted Marker Genes:", marker_genes_from_file)
+        else:
+            st.warning("The uploaded file is empty. Please check your file.")
+
+# Text area appears only if 'Custom Input' is selected
+elif st.session_state.background_context == "Custom Input":
+   st.session_state.custom_data = st.text_area("Enter custom gene dataset:")
+
+
+#selecting Species as global variable
+species = st.radio(
+      "Select Species",
+      ("Homo sapiens", "Mus musculus")
+   )
+if species == 'Homo sapiens':
+   st.session_state.species = "Homo sapiens"
+   df_selected = df_human
+   total_species_cells = human_cells
+else:
+   st.session_state.species = "Mus musculus"
+   df_selected = df_mouse
+   total_species_cells = mouse_cells
+
+#selecting Tissue as global variable
+tissue_options = get_all_tissues(df_selected, species)
+selected_tissues = st.multiselect(
+   "Select Tissue Type(s)",
+   options=tissue_options,
+   default=["All"]
+)
+st.session_state.tissue = selected_tissues
+
+#selcting Marker Genes as Global Variable
+marker_genes_input = st.text_area(
+    "Enter Marker Genes",
+    placeholder="Enter marker genes, separated by commas. Ex: Gpx2, Rps12, Rpl12, Eef1a1, Rps19, Rpsa, Rps3, "
+    "Rps26, Rps24, Rps28, Reg4, Cldn2, Cd24a, Zfas1, Stmn1, Kcnq1, Rpl36a-ps1, Hopx, Cdca7, Smoc2"
+)
+marker_genes = string_to_gene_array(marker_genes_input)
+st.session_state.marker_genes = marker_genes
+st.write("**To access pages 4 and 5, Hybrid and LLM OpenAI you will ned to enter an OpenAI API key**")
+st.session_state.api_key = st.text_input("Enter your OpenAI API key", type="password", placeholder="sk-...")
+
+if st.button("Save Selection"):
+   st.success(f"Selected option: {st.session_state.background_context}, {st.session_state.species}, {st.session_state.tissue}, {st.session_state.marker_genes}")
+   if (st.session_state.background_context == "Upload TSV File") and (st.session_state.Gene_denominator.size > 0):
+      st.success("File uploaded successfully!")
+       
