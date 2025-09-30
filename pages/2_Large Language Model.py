741f671cb01af0430d83a8d39d12ed91b59e9194
Sam J. Wood <Samuel.wood648693@tufts.edu>
2025-09-30 17:50:57 -0400

    Update 2_Large Language Model.py

--- pages/2_Large Language Model.py
+++ pages/2_Large Language Model.py
@@ -3,6 +3,11 @@ import streamlit as st
 import google.generativeai as genai
 import openai
 
+if "show_sidebar_pages" not in st.session_state or not st.session_state.show_sidebar_pages:
+    st.warning("This page is locked. Go to In-Depth Interface and click 'Save Selection' to enable navigation.")
+    st.stop()
+
+
 st.title("Large Language Model Cell Options Selector")
 
 if "species" not in st.session_state:
